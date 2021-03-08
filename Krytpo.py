b73de89fe24757c5111e618a55b7cb10968940f1
KrakenTW <ziolus03@gmail.com>
2021-03-08 02:14:31 +0100

    stworzenie okna aplikacji oraz podstawowego pola wprowadzającego text

--- Krytpo.py
+++ Krytpo.py
@@ -1,7 +1,34 @@
 import sys
+import tkinter
+import base64
+from tkinter import *
 
-nonchanged = open(r"C:\Users\kraken\Desktop\krytpo\Bezpiecze-stwo-sieci\nonchanged.txt")
-print(nonchanged.read()) 
 
+root = Tk()
+root.geometry('800x600')
+
+def button_command():
+    text = entry1.get()
+    print(text)
+    input = entry1.get()
+    return input
+
+def cryptography(input, key):
+    encoded_chars = []
+    for i in len(input):
+        key_c = key[i % len(key)]
+        encoded_c = chr(ord(input[i]) + ord(key_c) % 256)
+        encoded_chars.append(encoded_c)
+    encoded_string = "".join(encoded_chars)
+    return base64.urlsafe_b64encode(encoded_string)
+
+entry1 = Entry(root, width = 30)
+entry1.pack()
+Button(root, text="Tekst", command=button_command).pack()
 key = open(r"C:\Users\kraken\Desktop\krytpo\Bezpiecze-stwo-sieci\kod.txt")
-print(key.read()) 
+print(key.read())
+cryptography(input, key)
+
+
+root.mainloop()
+
