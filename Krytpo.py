aa39d63367c1aeb79690b78869476197c6e777f0
KrakenTW <ziolus03@gmail.com>
2021-03-08 01:13:35 +0100

    noncommit

---
+++ Krytpo.py
@@ -0,0 +1,7 @@
+import sys
+
+nonchanged = open(r"C:\Users\kraken\Desktop\krytpo\Bezpiecze-stwo-sieci\nonchanged.txt")
+print(nonchanged.read()) 
+
+key = open(r"C:\Users\kraken\Desktop\krytpo\Bezpiecze-stwo-sieci\kod.txt")
+print(key.read()) 
